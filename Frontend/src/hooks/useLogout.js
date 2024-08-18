1aae4f85507c72fb719f9df981675ff4ac1f0b8a
dnyaneshwar suwarnkar <sdnyaneshwar9421@gmail.com>
2024-08-18 18:38:37 +0530

    fix logout

--- Frontend/src/hooks/useLogout.js
+++ Frontend/src/hooks/useLogout.js
@@ -9,7 +9,7 @@ const useLogout =()=>{
 
     const logout = async()=>{
         try {
-            const res = await fetch("http://localhost:5000/api/users/logout",{
+            const res = await fetch("/api/users/logout",{
                 method:"POST",
                 headers:{
                     "Content-Type":"application/json"
