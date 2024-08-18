2b8d098991f33bc304ce172ed1fa81c63aeeec29
dnyaneshwar suwarnkar <sdnyaneshwar9421@gmail.com>
2024-08-18 17:16:10 +0530

    deployment

--- Backend/server.js
+++ Backend/server.js
@@ -18,7 +18,7 @@ connectDB();
 job.start();
 
 const PORT = process.env.PORT || 5000;
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -41,13 +41,13 @@ app.use("/api/messages", messageRoutes);
 
 // http://localhost:5000 => backend,frontend
 
-// if (process.env.NODE_ENV === "production") {
-// 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-// 	// react app
-// 	app.get("*", (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// 	});
-// }
+	// react app
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));
